494bb1ec75b554c43d1c3a377f2e98314da6b440
Sami Islam <islam.samipkr@gmail.com>
2025-09-10 20:09:30 -0400

    Create resourcegroup.tf

---
+++ resourcegroup.tf
@@ -0,0 +1,12 @@
+resource "azurerm_resource_group" "terraformmay022025" {
+  name     = "mcit_resource_group_may022025"
+  location = "canadacentral"
+}
+resource "azurerm_resource_group" "terraformseptember" {
+  name     = "mcit_resource_group_sep092025"
+  location = "canadacentral"
+}
+resource "azurerm_resource_group" "tfsept2025" {
+  name     = "mcit_si_sept_2025"
+  location = "canadacentral"
+}
