da31c5e2807d29d092e58c821fd84a80fb9efc16
Sami Islam <islam.samipkr@gmail.com>
2025-09-15 18:10:10 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -24,3 +24,8 @@ output "list" {
   value = split("-", "app-prod-01")
 }
 # → ["app", "prod", "01"]
+output "clean_name" {
+  value = replace("app-prod-01", "prod", "staging")
+}
+# → "app-staging-01"
+
