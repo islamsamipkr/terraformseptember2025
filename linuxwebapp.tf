450ea5dffa611f6697afc4be354611f1e3a9b215
Sami Islam <islam.samipkr@gmail.com>
2025-09-22 18:04:13 -0400

    Create linuxwebapp.tf

---
+++ linuxwebapp.tf
@@ -0,0 +1,29 @@
+resource "azurerm_resource_group" "mcitazurerm" {
+  name     = "septemberazurerm"
+  location = "Canada Central"
+}
+
+resource "azurerm_service_plan" "mcitsplan" {
+  name                = "mcitserviceplan"
+  resource_group_name = azurerm_resource_group.mcitazurerm.name
+  location            = azurerm_resource_group.mcitazurerm.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+}
+
+# Define a list of app names
+variable "webapp_names" {
+  type    = list(string)
+  default = ["app1", "app2", "app3", "app4", "app5"]
+}
+
+resource "azurerm_linux_web_app" "mcitlinuxwebapp" {
+  for_each            = toset(var.webapp_names)
+
+  name                = each.key
+  resource_group_name = azurerm_resource_group.mcitazurerm.name
+  location            = azurerm_service_plan.mcitazurerm.location
+  service_plan_id     = azurerm_service_plan.mcitsplan.id
+
+  site_config {}
+}
