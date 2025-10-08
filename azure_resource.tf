81941b4922515f3f229d50798ddd82538817b6f6
Sami Islam <islam.samipkr@gmail.com>
2025-10-08 19:39:24 -0400

    Update azure_resource.tf

--- azure_resource.tf
+++ azure_resource.tf
@@ -38,6 +38,30 @@ resource "azurerm_key_vault" "mcitprefix_kv" {
   soft_delete_retention_days = 7
 }
 
+resource "azurerm_machine_learning_workspace" "mcitprefix_ws" {
+  name                    = "${var.prefix}-ws"
+  location                = azurerm_resource_group.mcitprefix_rg.location
+  resource_group_name     = azurerm_resource_group.mcitprefix_rg.name
+  storage_account_id      = azurerm_storage_account.mcitprefix_sa.id
+  key_vault_id            = azurerm_key_vault.mcitprefix_kv.id
+  application_insights_id = azurerm_application_insights.mcitprefix_appi.id
+  container_registry_id   = azurerm_container_registry.mcitprefix_acr.id
+  identity { type = "SystemAssigned" }
+  public_network_access_enabled = true
+}
+
+resource "azurerm_machine_learning_compute_cluster" "mcitprefix_cpu" {
+  name                          = "${var.prefix}-cpu"
+  location                      = azurerm_resource_group.mcitprefix_rg.location
+  machine_learning_workspace_id = azurerm_machine_learning_workspace.mcitprefix_ws.id
+  vm_size                       = "STANDARD_DS3_V2"
+  scale_settings { min_node_count = 0, max_node_count = 1 }
+}
+
+output "workspace_name" {
+  value = azurerm_machine_learning_workspace.mcitprefix_ws.name
+}
+
 resource "azurerm_application_insights" "mcitprefix_appi" {
   name                = "${var.prefix}-appi"
   location            = azurerm_resource_group.mcitprefix_rg.location
@@ -64,17 +88,6 @@ resource "azurerm_key_vault_access_policy" "mcitprefix_kv_policy" {
   certificate_permissions = ["Get", "List"]
 }
 
-resource "azurerm_machine_learning_compute_cluster" "mcitprefix_cpu" {
-  name                = "${var.prefix}-cpu"
-  location            = azurerm_resource_group.mcitprefix_rg.location
-  resource_group_name = azurerm_resource_group.mcitprefix_rg.name
-  workspace_name      = azurerm_machine_learning_workspace.mcitprefix_ws.name
-  vm_size = "STANDARD_DS3_V2"
-  scale_settings {
-    min_node_count = 0
-    max_node_count = 1
-  }
-}
 
 output "resource_group" {
   value = azurerm_resource_group.mcitprefix_rg.name
