b14674723266282f983d2d105390ae89c7074d70
Sami Islam <islam.samipkr@gmail.com>
2025-10-08 19:41:32 -0400

    Update azure_resource.tf

--- azure_resource.tf
+++ azure_resource.tf
@@ -1,13 +1,3 @@
-variable "location" {
-  type    = string
-  default = "Canada Central"
-}
-
-variable "prefix" {
-  type    = string
-  default = "montrealitcollege"
-}
-
 
 resource "random_string" "mcitprefix_random" {
   length  = 6
@@ -21,11 +11,12 @@ resource "azurerm_resource_group" "mcitprefix_rg" {
 }
 
 resource "azurerm_storage_account" "mcitprefix_sa" {
-  name                     = "${var.prefix}sa${random_string.mcitprefix_random.result}"
-  resource_group_name      = azurerm_resource_group.mcitprefix_rg.name
-  location                 = azurerm_resource_group.mcitprefix_rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  name                         = "${var.prefix}sa${random_string.mcitprefix_random.result}"
+  resource_group_name          = azurerm_resource_group.mcitprefix_rg.name
+  location                     = azurerm_resource_group.mcitprefix_rg.location
+  account_tier                 = "Standard"
+  account_replication_type     = "LRS"
+  allow_nested_items_to_be_public = false
 }
 
 resource "azurerm_key_vault" "mcitprefix_kv" {
@@ -36,30 +27,6 @@ resource "azurerm_key_vault" "mcitprefix_kv" {
   sku_name                   = "standard"
   purge_protection_enabled   = true
   soft_delete_retention_days = 7
-}
-
-resource "azurerm_machine_learning_workspace" "mcitprefix_ws" {
-  name                    = "${var.prefix}-ws"
-  location                = azurerm_resource_group.mcitprefix_rg.location
-  resource_group_name     = azurerm_resource_group.mcitprefix_rg.name
-  storage_account_id      = azurerm_storage_account.mcitprefix_sa.id
-  key_vault_id            = azurerm_key_vault.mcitprefix_kv.id
-  application_insights_id = azurerm_application_insights.mcitprefix_appi.id
-  container_registry_id   = azurerm_container_registry.mcitprefix_acr.id
-  identity { type = "SystemAssigned" }
-  public_network_access_enabled = true
-}
-
-resource "azurerm_machine_learning_compute_cluster" "mcitprefix_cpu" {
-  name                          = "${var.prefix}-cpu"
-  location                      = azurerm_resource_group.mcitprefix_rg.location
-  machine_learning_workspace_id = azurerm_machine_learning_workspace.mcitprefix_ws.id
-  vm_size                       = "STANDARD_DS3_V2"
-  scale_settings { min_node_count = 0, max_node_count = 1 }
-}
-
-output "workspace_name" {
-  value = azurerm_machine_learning_workspace.mcitprefix_ws.name
 }
 
 resource "azurerm_application_insights" "mcitprefix_appi" {
@@ -77,7 +44,17 @@ resource "azurerm_container_registry" "mcitprefix_acr" {
   admin_enabled       = true
 }
 
-
+resource "azurerm_machine_learning_workspace" "mcitprefix_ws" {
+  name                        = "${var.prefix}-ws"
+  location                    = azurerm_resource_group.mcitprefix_rg.location
+  resource_group_name         = azurerm_resource_group.mcitprefix_rg.name
+  storage_account_id          = azurerm_storage_account.mcitprefix_sa.id
+  key_vault_id                = azurerm_key_vault.mcitprefix_kv.id
+  application_insights_id     = azurerm_application_insights.mcitprefix_appi.id
+  container_registry_id       = azurerm_container_registry.mcitprefix_acr.id
+  public_network_access_enabled = true
+  identity { type = "SystemAssigned" }
+}
 
 resource "azurerm_key_vault_access_policy" "mcitprefix_kv_policy" {
   key_vault_id = azurerm_key_vault.mcitprefix_kv.id
@@ -88,11 +65,18 @@ resource "azurerm_key_vault_access_policy" "mcitprefix_kv_policy" {
   certificate_permissions = ["Get", "List"]
 }
 
+resource "azurerm_machine_learning_compute_cluster" "mcitprefix_cpu" {
+  name                          = "${var.prefix}-cpu"
+  location                      = azurerm_resource_group.mcitprefix_rg.location
+  machine_learning_workspace_id = azurerm_machine_learning_workspace.mcitprefix_ws.id
+  vm_size                       = "STANDARD_DS3_V2"
 
-output "resource_group" {
-  value = azurerm_resource_group.mcitprefix_rg.name
+  scale_settings {
+    min_node_count = 0
+    max_node_count = 1
+  }
 }
 
-output "workspace_name" {
-  value = azurerm_machine_learning_workspace.mcitprefix_ws.name
-}
+output "resource_group_name" { value = azurerm_resource_group.mcitprefix_rg.name }
+output "workspace_name"      { value = azurerm_machine_learning_workspace.mcitprefix_ws.name }
+output "workspace_id"        { value = azurerm_machine_learning_workspace.mcitprefix_ws.id }
